0944ba7b84d40e485c972c1f14b5b1862d086fe9
Zainab Fatima <zainabfatima.zaman@gmail.com>
2025-05-17 13:42:43 +0530

    Add files via upload

---
+++ authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+export default authMiddleware;
